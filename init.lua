dc6f079845ee62b979f6bc0794326c3e72af92d1
bartp <bartek.paraf@gmail.com>
2022-03-30 12:15:48 +0200

    initial commit

---
+++ init.lua
@@ -0,0 +1,12 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.comment"
+require "user.gitsigns"
+require "user.treesitter"
+require "user.autopairs"
+
+
