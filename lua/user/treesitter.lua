04a0291bd862fd7326cf0c35121c71eee0758a3f
Bart-P <bartek.paraf@gmail.com>
2022-04-24 15:25:24 +0200

    switched to ensure_installed just because

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
@@ -13,8 +13,8 @@ configs.setup {
     additional_vim_regex_highlighting = true,
   },
   indent = { enable = true, disable = { "yaml" } },
-  autotag = { enable = true, },
+  autotag = { enable = true },
   context_commentstring = {
-    enable = true
+    enable = true,
   },
 }
