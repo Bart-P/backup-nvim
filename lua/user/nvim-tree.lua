78cce34a652eae15216a66f4a42d46c35a77a7fb
Bart-P <bartek.paraf@gmail.com>
2022-04-24 15:22:25 +0200

    removed depracated options

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -44,11 +44,7 @@ nvim_tree.setup({
 	},
 	open_on_tab = false,
 	hijack_cursor = false,
-	update_cwd = true,
-	update_to_buf_dir = {
-		enable = true,
-		auto_open = true,
-	},
+	update_cwd = false,
 	diagnostics = {
 		enable = true,
 		icons = {
@@ -68,12 +64,32 @@ nvim_tree.setup({
 		ignore = true,
 		timeout = 500,
 	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		open_file = {
+			quit_on_open = true,
+			resize_window = false,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+	},
 	view = {
 		width = 50,
 		height = 30,
 		hide_root_folder = false,
 		side = "left",
-		auto_resize = true,
+		-- auto_resize = true,
 		mappings = {
 			custom_only = false,
 			list = {
@@ -84,16 +100,5 @@ nvim_tree.setup({
 		},
 		number = false,
 		relativenumber = false,
-	},
-	quit_on_open = 0,
-	git_hl = 1,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
-	show_icons = {
-		git = 1,
-		folders = 1,
-		files = 1,
-		folder_arrows = 1,
-		tree_width = 30,
 	},
 })
